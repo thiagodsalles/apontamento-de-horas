cb9ba78e87abe64501b9e9100454449d73ddc086
Thiago <thiagodsalles@gmail.com>
2020-06-09 00:08:19 -0300

    bin

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :branch, :capistrano
 set :deploy_to, '/home/deploy/commodre'
 set :pty, true
 set :linked_files, %w{config/database.yml config/master.key}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{ log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, '2.6.3' # Should match ruby version
